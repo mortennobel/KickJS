abd25b749c1e6e3ccca0c9181194a4ada625d742
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2011-11-13 11:22:55 +0100

    Add more configuration options for creation of webgl context

--- build/kick-uncompressed-0.1.0.js
+++ build/kick-uncompressed-0.1.0.js
@@ -5476,9 +5476,7 @@ KICK.namespace = KICK.namespace || function (ns_string) {
                 initGL = function(){
                     for (i = webGlContextNames.length-1; i >= 0; i--) {
                         try {
-                            gl = canvas.getContext(webGlContextNames[i],{
-                                preserveDrawingBuffer: thisObj.config.preserveDrawingBuffer
-                            });
+                            gl = canvas.getContext(webGlContextNames[i],thisObj.config);
                             if (gl) {
                                 break;
                             }
@@ -5587,11 +5585,60 @@ KICK.namespace = KICK.namespace || function (ns_string) {
         /**
          * Allows grabbing the content of the canvas using canvasObj.toDataURL(...).<br>
          * Note that this has a performance penalty when enabled.<br>
-         * Default value is false
+         * Default value is false<br>
+         * WebGL spec:  If false, once the drawing buffer is presented as described in theDrawing Buffer section,
+         * the contents of the drawing buffer are cleared to their default values.
+         * All elements of the drawing buffer (color, depth and stencil) are cleared.
+         * If the value is true the buffers will not be cleared and will preserve their
+         * values until cleared or overwritten by the author.
          * @property enableDebugContext
          * @type Boolean
          */
         this.preserveDrawingBuffer = config.preserveDrawingBuffer || false;
+
+        /**
+         * WebGL spec:  Default: true. If the value is true, the drawing buffer has an alpha channel for the purposes
+         * of performing OpenGL destination alpha operations and compositing with the page. If the value is false, no
+         * alpha buffer is available.
+         * @property alpha
+         * @type Boolean
+         */
+        this.alpha = typeof config.alpha === 'boolean' ? config.alpha : true;
+
+        /**
+         * WebGL spec: Default: true. If the value is true, the drawing buffer has a depth buffer of at least 16 bits.
+         * If the value is false, no depth buffer is available.
+         * @property alpha
+         * @type Boolean
+         */
+        this.depth = typeof config.depth === 'boolean' ? config.depth : true;
+
+        /**
+         * WebGL spec: Default: false. If the value is true, the drawing buffer has a stencil buffer of at least 8 bits.
+         * If the value is false, no stencil buffer is available.
+         * @property stencil
+         * @type Boolean
+         */
+        this.stencil = typeof config.stencil === 'boolean' ? config.stencil : false;
+
+        /**
+         * WebGL spec: Default: true. If the value is true and the implementation supports antialiasing the drawing
+         * buffer will perform antialiasing using its choice of technique (multisample/supersample) and quality.
+         * If the value is false or the implementation does not support antialiasing, no antialiasing is performed.
+         * @property antialias
+         * @type Boolean
+         */
+        this.antialias = typeof config.antialias === 'boolean' ? config.antialias : true;
+
+        /**
+         * WebGL spec: Default: true. If the value is true the page compositor will assume the drawing buffer contains
+         * colors with premultiplied alpha. If the value is false the page compositor will assume that colors in the
+         * drawing buffer are not premultiplied. This flag is ignored if the alpha flag is false.
+         * See Premultiplied Alpha for more information on the effects of the premultipliedAlpha flag.
+         * @property premultipliedAlpha
+         * @type Boolean
+         */
+        this.premultipliedAlpha = typeof config.premultipliedAlpha === 'boolean' ? config.premultipliedAlpha : true;
 
         /**
          * Polling of canvas resize. Default is 0 (meaning not polling)
