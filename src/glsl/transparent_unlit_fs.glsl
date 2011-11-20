2a20fcbdda510c652253199be9ad9a5d2fb77c29
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2011-11-20 22:37:53 +0100

    Add transparent shaders

---
+++ src/glsl/transparent_unlit_fs.glsl
@@ -0,0 +1,12 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+varying vec2 vUv;
+
+uniform vec4 mainColor;
+uniform sampler2D mainTexture;
+
+void main(void)
+{
+    gl_FragColor = texture2D(mainTexture,vUv)*mainColor;
+}
