4013d88a3847cc6b9e7b63f3dfb77639da5b5969
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2012-01-28 21:10:43 +0100

    Change GLSL mainColor to always be vec4 (event though only xyz is used in non-transparent shaders)

--- src/glsl/Unlit_fs.glsl
+++ src/glsl/Unlit_fs.glsl
@@ -3,11 +3,11 @@ precision highp float;
 #endif
 varying vec2 vUv;
 
-uniform vec3 mainColor;
+uniform vec4 mainColor;
 uniform sampler2D mainTexture;
 
 void main(void)
 {
-    gl_FragColor = texture2D(mainTexture,vUv)*vec4(mainColor,1.0);
+    gl_FragColor = texture2D(mainTexture,vUv)*vec4(mainColor.xyz,1.0);
 }
  
