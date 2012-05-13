c2937d06bbfea0792ae89043398f4e4fad787d1c
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2012-05-13 15:17:54 +0200

    JSLint

--- src/js/obj.js
+++ src/js/obj.js
@@ -22,6 +22,7 @@
  */
 var KICK = KICK || {};
 KICK.namespace = function (ns_string) {
+    "use strict"; // force strict ECMAScript 5
     var parts = ns_string.split("."),
         parent = window,
         i;
@@ -62,7 +63,7 @@ KICK.namespace = function (ns_string) {
      * @return {Object} returns container object with the properties (mesh:[], gameObjects:[], materials:[])
      * @static
      */
-    importer.ObjImporter.import = function (objFileContent, engine, scene, rotate90x){
+    importer.ObjImporter.import = function (objFileContent, engine, scene, rotate90x) {
         var lines = objFileContent.split("\n"),
             linesLength = lines.length,
             vertices = [],
@@ -75,42 +76,49 @@ KICK.namespace = function (ns_string) {
             allMaterials = [],
             allMeshes = [],
             objectName = "MeshObject",
-            trim = function (str){ return str.replace(/^\s+|\s+$/g, ""); },
-            strAsArray = function(numberString, type){
-                if (!type){
+            i,
+            j,
+            trim = function (str) { return str.replace(/^\s+|\s+$/g, ""); },
+            strAsArray = function (numberString, type) {
+                if (!type) {
                     type = Array;
                 }
-                numberString = numberString.replace(/^\s+|\s+$/g,""); // trim
+                numberString = numberString.replace(/^\s+|\s+$/g, ""); // trim
                 numberString = numberString.replace(/\s{2,}/g, ' '); // remove double white spaces
                 var numberArray = numberString.split(" ").map(Number);
-                if (!type || type === Array){
+                if (!type || type === Array) {
                     return numberArray;
                 } else {
                     // typed array
                     return new type(numberArray);
                 }
             },
-            getIndices = function(strArray){
-                var array = [];
-                for (var i=0;i<strArray.length;i++){
-                    var str = strArray[i],
-                        splittedStr = str.split("/"),
-                        vertexIndex = parseInt(splittedStr[0]);
+            getIndices = function (strArray) {
+                var array = [],
+                    i,
+                    str,
+                    splittedStr,
+                    vertexIndex;
+                for (i = 0; i < strArray.length; i++) {
+                    str = strArray[i];
+                    splittedStr = str.split("/");
+                    vertexIndex = parseInt(splittedStr[0],10);
                     array.push([vertexIndex,
-                        splittedStr.length>=2 ? parseInt(splittedStr[1]) : vertexIndex,
-                        splittedStr.length>=3 ? parseInt(splittedStr[2]) : vertexIndex]);
+                        splittedStr.length >= 2 ? parseInt(splittedStr[1], 10) : vertexIndex,
+                        splittedStr.length >= 3 ? parseInt(splittedStr[2], 10) : vertexIndex]);
                 }
 
                 return array;
             },
-            addObject = function(){
-                var pushVertexData = function(source, index, dest){
-                    var sourceElement = source[index-1]; // note: obj is 1 indexed - therefor -1
-                    for (var i=0;i<sourceElement.length;i++){
+            addObject = function () {
+                var pushVertexData = function (source, index, dest) {
+                    var sourceElement = source[index - 1], // note: obj is 1 indexed - therefor -1
+                        i;
+                    for (i = 0; i < sourceElement.length; i++) {
                         dest.push(sourceElement[i]);
                     }
                 };
-                if (vertices.length==0){
+                if (vertices.length === 0) {
                     return;
                 }
                 var gameObject = scene.createGameObject(),
@@ -122,64 +130,70 @@ KICK.namespace = function (ns_string) {
                     meshDataIndices,
                     meshDataSubmeshes = [],
                     cache = {},
-                    count = 0;
+                    count = 0,
+                    vertexUvsNormalStrArray,
+                    idx,
+                    vertexUvsNormalStr,
+                    k,
+                    i,
+                    j;
                 allMeshes.push(mesh);
-                for (var k=0;k<submeshes.length;k++){
+                for (k = 0; k < submeshes.length; k++) {
                     triangles = submeshes[k];
                     meshDataIndices = [];
                     meshDataSubmeshes.push(meshDataIndices);
-                    for (var i=0;i<triangles.length;i++){
-                        var vertexUvsNormalStrArray = triangles[i]; // has the value such as ["1//1", "2//2", "3//3"]
-                        var idx = getIndices(vertexUvsNormalStrArray);
-                        for (var j=0;j<3;j++){
-                            var vertexUvsNormalStr = vertexUvsNormalStrArray[j]; // has the value such as "1//1"
-                            if (typeof cache[vertexUvsNormalStr] === 'number'){ // if index is in the cache, reuse index
+                    for (i = 0; i < triangles.length; i++) {
+                        vertexUvsNormalStrArray = triangles[i]; // has the value such as ["1//1", "2//2", "3//3"]
+                        idx = getIndices(vertexUvsNormalStrArray);
+                        for (j = 0; j < 3; j++) {
+                            vertexUvsNormalStr = vertexUvsNormalStrArray[j]; // has the value such as "1//1"
+                            if (typeof cache[vertexUvsNormalStr] === 'number') { // if index is in the cache, reuse index
                                 meshDataIndices.push(cache[vertexUvsNormalStr]);
                             } else {
-                                pushVertexData(vertices,idx[j][0],meshDataVertices);
-                                if (textureCoordinates.length){
-                                    pushVertexData(textureCoordinates,idx[j][1],meshDataTextureCoordinates);
+                                pushVertexData(vertices,idx[j][0], meshDataVertices);
+                                if (textureCoordinates.length) {
+                                    pushVertexData(textureCoordinates, idx[j][1], meshDataTextureCoordinates);
                                 }
-                                if (normals.length){
-                                    pushVertexData(normals,idx[j][2],meshDataNormals);
+                                if (normals.length) {
+                                    pushVertexData(normals, idx[j][2], meshDataNormals);
                                 }
                                 meshDataIndices.push(count);
                                 cache[vertexUvsNormalStr] = count;
-                                count ++;
+                                count++;
                             }
                         }
                     }
                 }
 
                 meshData.vertex = meshDataVertices;
-                if (meshDataNormals.length){
+                if (meshDataNormals.length) {
                     meshData.normal = meshDataNormals;
                 }
-                if (meshDataTextureCoordinates.length){
+                if (meshDataTextureCoordinates.length) {
                     meshData.uv1 = meshDataTextureCoordinates;
                 }
                 meshData.subMeshes = meshDataSubmeshes;
                 mesh.meshData = meshData;
-                mesh.name = objectName+" mesh";
+                mesh.name = objectName + " mesh";
                 var meshRenderer = new KICK.scene.MeshRenderer();
                 meshRenderer.mesh = mesh;
 
                 var materials = [];
 
-                var addDefaultMaterial = function(name){
+                var addDefaultMaterial = function (name) {
                     var newMaterial = new KICK.material.Material(engine,{
-                        name:name,
-                        shader:engine.project.load(engine.project.ENGINE_SHADER_DEFAULT)
+                        name: name,
+                        shader: engine.project.load(engine.project.ENGINE_SHADER_DEFAULT)
                     });
                     materials.push(newMaterial);
                     allMaterials.push(newMaterial);
                 };
 
-                for (var i=0;i < meshDataSubmeshes.length;i++){
-                    if (i<materialNames.length){
+                for (i = 0; i < meshDataSubmeshes.length; i++) {
+                    if (i < materialNames.length) {
                         var materialName = materialNames[i];
-                        var projectMaterial = engine.project.loadByName(materialName,"KICK.material.Material");
-                        if (projectMaterial){
+                        var projectMaterial = engine.project.loadByName(materialName, "KICK.material.Material");
+                        if (projectMaterial) {
                             materials.push(projectMaterial);
                         } else {
                             addDefaultMaterial(materialName);
@@ -197,49 +211,49 @@ KICK.namespace = function (ns_string) {
             };
 
         var transformMatrix = mat4.identity(mat4.create());
-        if (rotate90x){
-            mat4.rotateX(transformMatrix,-90*KICK.core.Constants._DEGREE_TO_RADIAN);
+        if (rotate90x) {
+            mat4.rotateX(transformMatrix, -90 * KICK.core.Constants._DEGREE_TO_RADIAN);
         }
 
-        for (var i=0;i<linesLength;i++){
+        for (i = 0;i < linesLength; i++) {
             var line = trim(lines[i]);
             var tokenIndex = line.indexOf(' ');
-            if (tokenIndex<0){
+            if (tokenIndex < 0) {
                 continue;
             }
-            var token = line.substring(0,tokenIndex);
-            var value = line.substring(tokenIndex+1);
-            if (token === "o"){
+            var token = line.substring(0, tokenIndex);
+            var value = line.substring(tokenIndex + 1);
+            if (token === "o") {
                 addObject();
                 objectName = value;
                 materialNames.length = 0;
-            } else if (token === "usemtl"){
+            } else if (token === "usemtl") {
                 materialNames.push(value);
                 // create material with name value is not exist
-                if (triangles.length>0){
+                if (triangles.length > 0) {
                     triangles = [];
                     submeshes[submeshes.length] = triangles;
                 }
-            } else if (token === "v"){
+            } else if (token === "v") {
                 var vertex = strAsArray(value);
-                mat4.multiplyVec3(transformMatrix,vertex);
+                mat4.multiplyVec3(transformMatrix, vertex);
                 vertices.push(vertex);
-            } else if (token === "vn"){
+            } else if (token === "vn") {
                 var normal = strAsArray(value);
                 mat4.multiplyVec3Vector(transformMatrix,normal);
                 normals.push(normal);
-            } else if (token === "vt"){
+            } else if (token === "vt") {
                 textureCoordinates.push(strAsArray(value));
-            } else if (token === "f"){
+            } else if (token === "f") {
                 var polygon = value.split(" ");
-                triangles.push(polygon.slice(0,3));
-                for (var j=3;j<polygon.length;j++){
-                    triangles.push([polygon[j-1],polygon[j],polygon[0]]);
+                triangles.push(polygon.slice(0, 3));
+                for (j = 3; j < polygon.length; j++) {
+                    triangles.push([polygon[j - 1], polygon[j], polygon[0]]);
                 }
             }
         }
         addObject();
 
-        return {mesh:allMeshes, gameObjects:allGameObjects, materials:allMaterials};
+        return {mesh: allMeshes, gameObjects: allGameObjects, materials: allMaterials};
     };
-})();
+}());
