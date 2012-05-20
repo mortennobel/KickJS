42b8afc969aca033c9e4a9b2ad91fda4f760578b
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2012-05-20 10:44:04 +0200

    Clean up code

--- src/js/meshfactory.js
+++ src/js/meshfactory.js
@@ -22,6 +22,7 @@
  */
 var KICK = KICK || {};
 KICK.namespace = function (ns_string) {
+    "use strict"; // force strict ECMAScript 5
     var parts = ns_string.split("."),
         parent = window,
         i;
@@ -61,24 +62,24 @@ KICK.namespace = function (ns_string) {
          */
         createTriangleData : function () {
             var sqrt75 = Math.sqrt(0.75);
-            return new mesh.MeshData( {
+            return new mesh.MeshData({
                 name: "Triangle",
                 vertex: [
-                    0,1,0,
-                    -sqrt75,-0.5,0,
-                    sqrt75,-0.5,0
+                    0, 1, 0,
+                    -sqrt75, -0.5, 0,
+                    sqrt75, -0.5, 0
                 ],
                 uv1: [
-                    0.5,1,
-                    0.125,0.25,
-                    1-0.125,0.25
+                    0.5, 1,
+                    0.125, 0.25,
+                    1 - 0.125, 0.25
                 ],
                 normal: [
-                    0,0,1,
-                    0,0,1,
-                    0,0,1
+                    0, 0, 1,
+                    0, 0, 1,
+                    0, 0, 1
                 ],
-                indices: [0,1,2]
+                indices: [0, 1, 2]
             });
         },
 
@@ -92,25 +93,25 @@ KICK.namespace = function (ns_string) {
             return new mesh.MeshData({
                 name: "Plane",
                 vertex: [
-                    1,-1,0,
-                    1,1,0,
-                    -1,-1,0,
-                    -1,1,0
+                    1, -1, 0,
+                    1, 1, 0,
+                    -1, -1, 0,
+                    -1, 1, 0
 
                 ],
                 uv1: [
-                    1,0,
-                    1,1,
-                    0,0,
-                    0,1
+                    1, 0,
+                    1, 1,
+                    0, 0,
+                    0, 1
                 ],
                 normal: [
-                    0,0,1,
-                    0,0,1,
-                    0,0,1,
-                    0,0,1
+                    0, 0, 1,
+                    0, 0, 1,
+                    0, 0, 1,
+                    0, 0, 1
                 ],
-                indices: [0,1,2,2,1,3]
+                indices: [0, 1, 2, 2, 1, 3]
             });
         },
 
@@ -123,69 +124,74 @@ KICK.namespace = function (ns_string) {
          * @param {Number} radius
          * @return {KICK.mesh.MeshData} uv-sphere mesh
          */
-        createUVSphereData : function(slices, stacks, radius){
-            if (!slices || slices < 3){
+        createUVSphereData : function (slices, stacks, radius) {
+            if (!slices || slices < 3) {
                 slices = 64;
             }
-            if (!stacks || stacks < 2){
+            if (!stacks || stacks < 2) {
                 stacks = 32;
             }
-            if (!radius){
+            if (!radius) {
                 radius = 1;
             }
-            var vertexCount =
-                stacks*(slices+1)*2+
-                2*(stacks-1), // degenerate vertex info
+            var j, i,
+                vertexCount = stacks * (slices + 1) * 2 + 2 * (stacks - 1), // degenerate vertex info
                 normalsMemory = {},
-                normals = vec3.array(vertexCount,normalsMemory),
+                normals = vec3.array(vertexCount, normalsMemory),
                 verticesMemory = {},
-                vertices = vec3.array(vertexCount,verticesMemory),
+                vertices = vec3.array(vertexCount, verticesMemory),
                 uvsMemory = {},
-                uvs = vec2.array(vertexCount,uvsMemory),
+                uvs = vec2.array(vertexCount, uvsMemory),
                 indices = [],
-                piDivStacks = Math.PI/stacks,
-                PIDiv2 = Math.PI/2,
-                PI2 = Math.PI*2;
+                piDivStacks = Math.PI / stacks,
+                PIDiv2 = Math.PI / 2,
+                PI2 = Math.PI * 2,
+                index = 0,
+                latitude1, latitude2,
+                sinLat1, sinLat2,
+                cosLat1, cosLat2,
+                longitude,
+                sinLong, cosLong,
+                x1, x2, y1, y2, z1, z2,
+                meshDataConf;
 
-            var index = 0;
-
-            for (var j = 0; j < stacks; j++) {
-                var latitude1 = piDivStacks * j - PIDiv2;
-                var latitude2 = piDivStacks * (j+1) - PIDiv2;
-                var sinLat1 = Math.sin(latitude1);
-                var cosLat1 = Math.cos(latitude1);
-                var sinLat2 = Math.sin(latitude2);
-                var cosLat2 = Math.cos(latitude2);
-                for (var i = 0; i <= slices; i++) {
-                    var longitude = (PI2/slices) * i;
-                    var sinLong = Math.sin(longitude);
-                    var cosLong = Math.cos(longitude);
-                    var x1 = cosLong * cosLat1;
-                    var y1 = sinLat1;
-                    var z1 = sinLong * cosLat1;
-                    var x2 = cosLong * cosLat2;
-                    var y2 = sinLat2;
-                    var z2 = sinLong * cosLat2;
-                    vec3.set([x1,y1,z1],normals[index]);
-                    vec2.set([1-i/slices, j/stacks ],uvs[index]);
-                    vec3.set([radius*x1,radius*y1,radius*z1],vertices[index]);
+            for (j = 0; j < stacks; j++) {
+                latitude1 = piDivStacks * j - PIDiv2;
+                latitude2 = piDivStacks * (j + 1) - PIDiv2;
+                sinLat1 = Math.sin(latitude1);
+                cosLat1 = Math.cos(latitude1);
+                sinLat2 = Math.sin(latitude2);
+                cosLat2 = Math.cos(latitude2);
+                for (i = 0; i <= slices; i++) {
+                    longitude = (PI2 / slices) * i;
+                    sinLong = Math.sin(longitude);
+                    cosLong = Math.cos(longitude);
+                    x1 = cosLong * cosLat1;
+                    y1 = sinLat1;
+                    z1 = sinLong * cosLat1;
+                    x2 = cosLong * cosLat2;
+                    y2 = sinLat2;
+                    z2 = sinLong * cosLat2;
+                    vec3.set([x1, y1, z1], normals[index]);
+                    vec2.set([1 - i / slices, j / stacks], uvs[index]);
+                    vec3.set([radius * x1, radius * y1, radius * z1], vertices[index]);
                     indices.push(index);
-                    if (j>0 && i==0){
+                    if (j > 0 && i === 0) {
                         indices.push(index); // make degenerate
                     }
                     index++;
 
-                    vec3.set([x2,y2,z2],normals[index]);
-                    vec2.set([ 1-i /slices, (j+1)/stacks],uvs[index]);
-                    vec3.set([radius*x2,radius*y2,radius*z2],vertices[index]);
+                    vec3.set([x2, y2, z2], normals[index]);
+                    vec2.set([1 - i / slices, (j + 1) / stacks], uvs[index]);
+                    vec3.set([radius * x2, radius * y2, radius * z2], vertices[index]);
                     indices.push(index);
-                    if (i==slices && j<stacks-1){
+                    if (i === slices && j < stacks - 1) {
                         indices.push(index); // make degenerate
                     }
                     index++;
                 }
             }
-            var meshDataConf = {
+            meshDataConf = {
                 name: "UVSphere",
                 vertex: verticesMemory.mem,
                 uv1: uvsMemory.mem,
@@ -205,7 +211,7 @@ KICK.namespace = function (ns_string) {
          * @return {KICK.mesh.Mesh} cube mesh
          */
         createCubeData : function (length) {
-            if (!length){
+            if (!length) {
                 length = 1;
             }
 
@@ -219,124 +225,124 @@ KICK.namespace = function (ns_string) {
             var meshDataConf = {
                 name: "Cube",
                 vertex: [
-                   length,length,length,
-                   -length,length,length,
-                   -length,-length,length,
-                   length,-length,length,        // v0-v1-v2-v3
-                   length,length,length,
-                   length,-length,length,
-                   length,-length,-length,
-                   length,length,-length,        // v0-v3-v4-v5
-                   length,length,length,
-                   length,length,-length,
-                   -length,length,-length,
-                   -length,length,length,        // v0-v5-v6-v1
-                   -length,length,length,
-                   -length,length,-length,
-                   -length,-length,-length,
-                   -length,-length,length,    // v1-v6-v7-v2
-                   -length,-length,-length,
-                   length,-length,-length,
-                   length,-length,length,
-                   -length,-length,length,    // v7-v4-v3-v2
-                   length,-length,-length,
-                   -length,-length,-length,
-                    -length,length,-length,
-                    length,length,-length   // v4-v7-v6-v5
+                    length, length, length,
+                    -length, length, length,
+                    -length, -length, length,
+                    length, -length, length,        // v0-v1-v2-v3
+                    length, length, length,
+                    length, -length, length,
+                    length, -length, -length,
+                    length, length, -length,        // v0-v3-v4-v5
+                    length, length, length,
+                    length, length, -length,
+                    -length, length, -length,
+                    -length, length, length,        // v0-v5-v6-v1
+                    -length, length, length,
+                    -length, length, -length,
+                    -length, -length, -length,
+                    -length, -length, length,    // v1-v6-v7-v2
+                    -length, -length, -length,
+                    length, -length, -length,
+                    length, -length, length,
+                    -length, -length, length,    // v7-v4-v3-v2
+                    length, -length, -length,
+                    -length, -length, -length,
+                    -length, length, -length,
+                    length, length, -length   // v4-v7-v6-v5
                 ],
                 uv1: [
-                    1,1,
-                    0,1,
-                    0,0,
-                    1,0,                    // v0-v1-v2-v3
-                    0,1,
-                    0,0,
-                    1,0,
-                    1,1,              // v0-v3-v4-v5
-                    1,0,
-                    1,1,
-                    0,1,
-                    0,0,              // v0-v5-v6-v1 (top)
-                    1,1,
-                    0,1,
-                    0,0,
-                    1,0,              // v1-v6-v7-v2
-                    1,1,
-                    0,1,
-                    0,0,
-                    1,0,              // v7-v4-v3-v2 (bottom)
-                    0,0,
-                    1,0,
-                    1,1,
-                    0,1             // v4-v7-v6-v5
+                    1, 1,
+                    0, 1,
+                    0, 0,
+                    1, 0,                    // v0-v1-v2-v3
+                    0, 1,
+                    0, 0,
+                    1, 0,
+                    1, 1,              // v0-v3-v4-v5
+                    1, 0,
+                    1, 1,
+                    0, 1,
+                    0, 0,              // v0-v5-v6-v1 (top)
+                    1, 1,
+                    0, 1,
+                    0, 0,
+                    1, 0,              // v1-v6-v7-v2
+                    1, 1,
+                    0, 1,
+                    0, 0,
+                    1, 0,              // v7-v4-v3-v2 (bottom)
+                    0, 0,
+                    1, 0,
+                    1, 1,
+                    0, 1             // v4-v7-v6-v5
                 ],
                 normal: [
-                    0,0,1,
-                    0,0,1,
-                    0,0,1,
-                    0,0,1,             // v0-v1-v2-v3
-                    1,0,0,
-                    1,0,0,
-                    1,0,0,
-                    1,0,0,              // v0-v3-v4-v5
-                    0,1,0,
-                    0,1,0,
-                    0,1,0,
-                    0,1,0,              // v0-v5-v6-v1
-                    -1,0,0,
-                    -1,0,0,
-                    -1,0,0,
-                    -1,0,0,          // v1-v6-v7-v2
-                    0,-1,0,
-                    0,-1,0,
-                    0,-1,0,
-                    0,-1,0,         // v7-v4-v3-v2
-                    0,0,-1,
-                    0,0,-1,
-                    0,0,-1,
-                    0,0,-1        // v4-v7-v6-v5
+                    0, 0, 1,
+                    0, 0, 1,
+                    0, 0, 1,
+                    0, 0, 1,             // v0-v1-v2-v3
+                    1, 0, 0,
+                    1, 0, 0,
+                    1, 0, 0,
+                    1, 0, 0,              // v0-v3-v4-v5
+                    0, 1, 0,
+                    0, 1, 0,
+                    0, 1, 0,
+                    0, 1, 0,              // v0-v5-v6-v1
+                    -1, 0, 0,
+                    -1, 0, 0,
+                    -1, 0, 0,
+                    -1, 0, 0,          // v1-v6-v7-v2
+                    0, -1, 0,
+                    0, -1, 0,
+                    0, -1, 0,
+                    0, -1, 0,         // v7-v4-v3-v2
+                    0, 0, -1,
+                    0, 0, -1,
+                    0, 0, -1,
+                    0, 0, -1        // v4-v7-v6-v5
                 ],
                 color: [
-                    1,1,1,1,
-                    1,1,0,1,
-                    1,0,0,1,
-                    1,0,1,1,              // v0-v1-v2-v3
-                    1,1,1,1,
-                    1,0,1,1,
-                    0,0,1,1,
-                    0,1,1,1,              // v0-v3-v4-v5
-                    1,1,1,1,
-                    0,1,1,1,
-                    0,1,0,1,
-                    1,1,0,1,              // v0-v5-v6-v1
-                    1,1,0,1,
-                    0,1,0,1,
-                    0,0,0,1,
-                    1,0,0,1,              // v1-v6-v7-v2
-                    0,0,0,1,
-                    0,0,1,1,
-                    1,0,1,1,
-                    1,0,0,1,              // v7-v4-v3-v2
-                    0,0,1,1,
-                    0,0,0,1,
-                    0,1,0,1,
-                    0,1,1,1             // v4-v7-v6-v5
+                    1, 1, 1, 1,
+                    1, 1, 0, 1,
+                    1, 0, 0, 1,
+                    1, 0, 1, 1,              // v0-v1-v2-v3
+                    1, 1, 1, 1,
+                    1, 0, 1, 1,
+                    0, 0, 1, 1,
+                    0, 1, 1, 1,              // v0-v3-v4-v5
+                    1, 1, 1, 1,
+                    0, 1, 1, 1,
+                    0, 1, 0, 1,
+                    1, 1, 0, 1,              // v0-v5-v6-v1
+                    1, 1, 0, 1,
+                    0, 1, 0, 1,
+                    0, 0, 0, 1,
+                    1, 0, 0, 1,              // v1-v6-v7-v2
+                    0, 0, 0, 1,
+                    0, 0, 1, 1,
+                    1, 0, 1, 1,
+                    1, 0, 0, 1,              // v7-v4-v3-v2
+                    0, 0, 1, 1,
+                    0, 0, 0, 1,
+                    0, 1, 0, 1,
+                    0, 1, 1, 1             // v4-v7-v6-v5
                 ],
                 indices: [
-                    0,1,2,
-                    0,2,3,
-                    4,5,6,
-                    4,6,7,
-                    8,9,10,
-                    8,10,11,
-                    12,13,14,
-                    12,14,15,
-                    16,17,18,
-                    16,18,19,
-                    20,21,22,
-                    20,22,23]
+                    0, 1, 2,
+                    0, 2, 3,
+                    4, 5, 6,
+                    4, 6, 7,
+                    8, 9, 10,
+                    8, 10, 11,
+                    12, 13, 14,
+                    12, 14, 15,
+                    16, 17, 18,
+                    16, 18, 19,
+                    20, 21, 22,
+                    20, 22, 23]
             };
             return new mesh.MeshData(meshDataConf);
         }
     };
-})();
+}());
