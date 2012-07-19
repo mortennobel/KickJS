46f40aada30ffecf8e18227829d811364e0fc54a
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2012-07-19 22:48:39 +0200

    Well documented WebGL state object

--- src/js/core.js
+++ src/js/core.js
@@ -62,7 +62,7 @@ KICK.namespace = function (ns_string) {
      * @param {KICK.core.Config} config Optional, configuration object
      */
     core.Engine = function (idOrElement, config) {
-        var glState = {},
+        var glState = new core.GLState(),
             gl = null,
             canvas = typeof idOrElement === 'string' ? document.getElementById(idOrElement) : idOrElement,
             webGlContextNames = ["experimental-webgl", "webgl"],
@@ -115,7 +115,7 @@ KICK.namespace = function (ns_string) {
             /**
              * The WebGL state(readonly). (Only used to keep track on webgl state across different objects)
              * @property glState
-             * @type Object
+             * @type KICK.core.GLState
              * @protected
              */
             glState: {
@@ -503,6 +503,100 @@ KICK.namespace = function (ns_string) {
 
             thisObj._gameLoop(lastTime);
         }());
+    };
+
+    /**
+     * This object should only be used by advanced users.
+     * <br>
+     * The GLState object contains properties representing the current WebGL states. This object is useful
+     * when creating component with custom render methods. It is important that any state modified by such
+     * method needs to be reset (set to null).
+     * @class GLState
+     * @constructor
+     * @namespace KICK.core
+     * @param {KICK.core.Engine} engine
+     */
+    core.GLState = function () {
+        /**
+         * The current clear color
+         * @property currentClearColor
+         * @type KICK.math.vec4
+         */
+        this.currentClearColor = null;
+        /**
+         * Current bound mesh buffer
+         * @property meshBuffer
+         * @type WebGLBuffer
+         */
+        this.meshBuffer = null;
+        /**
+         * The shader bound by the current mesh
+         * @property meshShader
+         * @type KICK.material.Shader
+         */
+        this.meshShader = null;
+        /**
+         * Represents the current rendertarget state
+         * @property renderTarget
+         * @type KICK.texture.RenderTexture
+         */
+        this.renderTarget = null;
+        /**
+         * Represents the current shader bound
+         * @property boundShader
+         * @type KICK.material.Shader
+         */
+        this.boundShader = null;
+        /**
+         * Represent the material used
+         * @property currentMaterial
+         * @type KICK.material.Material
+         */
+        this.currentMaterial = null;
+
+        /**
+         * Represent the state of CULL_FACE (enabled / disabled) and cullFace (). Values must be one of:
+         * GL_FRONT, GL_FRONT_AND_BACK, GL_BACK or GL_NONE. (If none CULL_FACE is disabled otherwise enabled)
+         * @property faceCulling
+         * @type Number
+         */
+        this.faceCulling = null;
+
+        /**
+         * Represents the current depthFunc used. Must be one of the following values:
+         * GL_NEVER, GL_LESS, GL_EQUAL, GL_LEQUAL, GL_GREATER, GL_NOTEQUAL, GL_GEQUAL or GL_ALWAYS.
+         * @property zTest
+         * @type Number
+         */
+        this.zTest = null;
+
+        /**
+         * Represents the current depthMask state.
+         * @property depthMaskCache
+         * @type Boolean
+         */
+        this.depthMaskCache = null;
+
+        /**
+         * Represents if blend is enabled and the current s-factor and d-factor
+         * @property blendKey
+         * @type Object
+         */
+        this.blendKey = null;
+
+        /**
+         * Represents state of polygon offset fill
+         * @property polygonOffsetEnabled
+         * @type Boolean
+         */
+        this.polygonOffsetEnabled = null;
+
+        /**
+         * The size of the current viewport
+         * @property viewportSize
+         * @type KICK.math.vec2
+         */
+        this.viewportSize = null;
     };
 
     /**
