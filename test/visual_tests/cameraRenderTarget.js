36f11148bd5796a7ca9ab7de60aaa2e13811e3eb
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-01-14 22:26:25 +0100

    test files

--- test/visual_tests/cameraRenderTarget.js
+++ test/visual_tests/cameraRenderTarget.js
@@ -8,123 +8,114 @@ requirejs.config({
 requirejs(['kick/core/Engine', 'kick/scene/Camera', 'kick/material/Material', 'kick/material/Shader', 'kick/scene/MeshRenderer', 'kick/mesh/Mesh', "kick/core/Constants", "kick/texture/RenderTexture", "kick/texture/Texture", "kick/math"],
     function (Engine, Camera, Material, Shader, MeshRenderer, Mesh, Constants, RenderTexture, Texture, math) {
         "use strict";
-        var engine;
-        var meshRenderer;
+        var engine,
+            meshRenderer,
+            log = function (output) {
+                var element = document.getElementById("output");
+                element.innerHTML = element.innerHTML + output + "<br>";
+            },
+            clearLog = function () {
+                var element = document.getElementById("output");
+                element.innerHTML = "";
+            };
 
-        var log = function (output) {
-            var element = document.getElementById("output");
-            element.innerHTML = element.innerHTML+output+"<br>";
-        };
-
-        var clearLog = function(){
-            var element = document.getElementById("output");
-            element.innerHTML = "";
-        };
-
-        function setMaterial(vertexShaderId, fragmentShaderId, meshRenderer, materialUniforms){
+        function setMaterial(vertexShaderId, fragmentShaderId, meshRenderer, materialUniforms) {
             clearLog();
-            var vs = document.getElementById(vertexShaderId).value;
-            var fs = document.getElementById(fragmentShaderId).value;
-            var shader = new Shader();
+            var vs = document.getElementById(vertexShaderId).value,
+                fs = document.getElementById(fragmentShaderId).value,
+                shader = new Shader(),
+                missingAttributes;
             shader.vertexShaderSrc = vs;
             shader.fragmentShaderSrc = fs;
             shader.errorLog = log;
             shader.apply();
-            var missingAttributes = meshRenderer.mesh.verify(shader);
+            missingAttributes = meshRenderer.mesh.verify(shader);
             if (missingAttributes) {
                 log("Missing attributes in mesh " + JSON.stringify(missingAttributes));
                 return;
             }
 
             meshRenderer.material = new Material({
-                name:"Some material",
-                shader:shader,
+                name: "Some material",
+                shader: shader,
                 uniformData: materialUniforms
             });
         }
 
-        function recalculateNormals(){
-            clearLog();
-            var mesh = meshRenderer.mesh;
-            mesh.recalculateNormals();
-            mesh.updateData();
-        }
-
-        function recalculateTangents() {
-            clearLog();
-            var mesh = meshRenderer.mesh;
-            mesh.recalculateTangents();
-            mesh.updateData();
-        }
-
-        function addRotatorComponent(gameObject){
+        function addRotatorComponent(gameObject) {
             var time = engine.time,
-                    transform = gameObject.transform,
-                    rotationSpeed = 0.001,
-                    translation = transform.localPosition,
-                    rotVec = transform.localRotationEuler,
-                    radius = 5,
-                    radianToDegree = Constants._RADIAN_TO_DEGREE,
-                    res = document.getElementById("res");
+                transform = gameObject.transform,
+                rotationSpeed = 0.001,
+                translation = transform.localPosition,
+                rotVec = transform.localRotationEuler,
+                radius = 5,
+                radianToDegree = Constants._RADIAN_TO_DEGREE,
+                res = document.getElementById("res");
             gameObject.addComponent({
-                update: function(){
+                update: function () {
                     var timeTotal = time.time,
-                            rot = timeTotal*rotationSpeed;
-                    translation[0] = Math.sin(rot)*radius;
-                    translation[1] = Math.sin(rot*3);
-                    translation[2] = Math.cos(rot)*radius;
-                    rotVec[1] = rot*radianToDegree;
+                        rot = timeTotal * rotationSpeed;
+                    translation[0] = Math.sin(rot) * radius;
+                    translation[1] = Math.sin(rot * 3);
+                    translation[2] = Math.cos(rot) * radius;
+                    rotVec[1] = rot * radianToDegree;
                     transform.localPosition = translation;
-
+                    console.log(rotVec);
                     //new Date().getMilliseconds();
                     transform.localRotationEuler = rotVec;
-                    res.innerHTML = math.Mat4.strPretty(transform.getGlobalMatrix())+"\nRotation euler:"+math.Vec3.str(rotVec);
+                    res.innerHTML = math.Mat4.strPretty(transform.getGlobalMatrix()) + "\nRotation euler:" + math.Vec3.str(rotVec);
                 }
             });
         }
 
         function initKick() {
-            engine = new Engine('canvas',{
+            engine = new Engine('canvas', {
                 enableDebugContext: true
             });
 
-            var cameraObject = engine.activeScene.createGameObject();
+            var cameraObject = engine.activeScene.createGameObject(),
+                cameraObject2 = engine.activeScene.createGameObject(),
+                texture = new Texture(),
+                renderTexture,
+                camera,
+                camera2,
+                gameObject,
+                gameObject2,
+                meshRenderer2;
 
-            var cameraObject2 = engine.activeScene.createGameObject();
+            cameraObject2.transform.position = [0, 0, 3];
 
-            cameraObject2.transform.position = [0,0,3];
 
-            var texture = new Texture();
-            texture.setImageData(512,512,0,Constants.GL_UNSIGNED_BYTE,null,"");
-            var renderTexture = new RenderTexture({colorTexture:texture});
+            texture.setImageData(512, 512, 0, Constants.GL_UNSIGNED_BYTE, null, "");
+            renderTexture = new RenderTexture({colorTexture: texture});
 
-            var camera = new Camera({
-                clearColor: [0,0,0,1],
-                fieldOfView:60,
-                layerMask:1,
+            camera = new Camera({
+                clearColor: [0, 0, 0, 1],
+                fieldOfView: 60,
+                layerMask: 1,
                 renderTarget : renderTexture
             });
             cameraObject.addComponent(camera);
-            var camera2 = new Camera({
-                clearColor: [.1,.3,.6,1],
-                fieldOfView:60,
-                cameraIndex:2,
+            camera2 = new Camera({
+                clearColor: [0.1, 0.3, 0.6, 1],
+                fieldOfView: 60,
+                cameraIndex: 2,
                 layerMask: 2
             });
             cameraObject2.addComponent(camera2);
-            var gameObject = engine.activeScene.createGameObject();
+            gameObject = engine.activeScene.createGameObject();
             meshRenderer = new MeshRenderer();
             meshRenderer.mesh = new Mesh({dataURI: "kickjs://mesh/cube/?length=0.5"});
-            setMaterial('vertexShaderColor','fragmentShader',meshRenderer);
+            setMaterial('vertexShaderColor', 'fragmentShader', meshRenderer);
             gameObject.addComponent(meshRenderer);
 
-            var gameObject2 = engine.activeScene.createGameObject();
+            gameObject2 = engine.activeScene.createGameObject();
             gameObject2.layer = 2;
-            var meshRenderer2 = new MeshRenderer();
+            meshRenderer2 = new MeshRenderer();
             meshRenderer2.mesh = new Mesh({dataURI: "kickjs://mesh/cube/?length=0.5"});
-            setMaterial('vertexShaderTex','fragmentShaderTex',meshRenderer2, {
-                    tex:  texture
-                });
+            setMaterial('vertexShaderTex', 'fragmentShaderTex', meshRenderer2, {
+                tex:  texture
+            });
             gameObject2.addComponent(meshRenderer2);
 
             addRotatorComponent(cameraObject);
@@ -132,8 +123,7 @@ requirejs(['kick/core/Engine', 'kick/scene/Camera', 'kick/material/Material', 'k
         }
         initKick();
 
-        function UVSphere(){
-            meshRenderer.mesh =  new Mesh({dataURI:"kickjs://mesh/uvsphere/"});
+        function UVSphere() {
+            meshRenderer.mesh = new Mesh({dataURI: "kickjs://mesh/uvsphere/"});
         }
-    }
-);
+    });
--- test/visual_tests/cameraViewportTest.js
+++ test/visual_tests/cameraViewportTest.js
@@ -8,55 +8,54 @@ requirejs.config({
 requirejs(['kick'],
     function (KICK) {
         "use strict";
-        var engine;
-        var meshRenderer;
-
-        var log = function (output) {
-            var element = document.getElementById("output");
-            element.innerHTML = element.innerHTML+output+"<br>";
-        };
-
-        var clearLog = function(){
-            var element = document.getElementById("output");
-            element.innerHTML = "";
-        };
+        var engine,
+            meshRenderer,
+            log = function (output) {
+                var element = document.getElementById("output");
+                element.innerHTML = element.innerHTML + output + "<br>";
+            },
+            clearLog = function () {
+                var element = document.getElementById("output");
+                element.innerHTML = "";
+            };
 
         function setMaterial(vertexShaderId, fragmentShaderId, meshRenderer){
             clearLog();
-            var vs = document.getElementById(vertexShaderId).value;
-            var fs = document.getElementById(fragmentShaderId).value;
-            var shader = new KICK.material.Shader();
+            var vs = document.getElementById(vertexShaderId).value,
+                fs = document.getElementById(fragmentShaderId).value,
+                shader = new KICK.material.Shader(),
+                missingAttributes;
             shader.vertexShaderSrc = vs;
             shader.fragmentShaderSrc = fs;
             shader.errorLog = log;
             shader.apply();
-            var missingAttributes = meshRenderer.mesh.verify(shader);
-            if (missingAttributes){
-                log("Missing attributes in mesh "+JSON.stringify(missingAttributes));
+            missingAttributes = meshRenderer.mesh.verify(shader);
+            if (missingAttributes) {
+                log("Missing attributes in mesh " + JSON.stringify(missingAttributes));
                 return;
             }
 
             meshRenderer.material = new KICK.material.Material({
-                name:"Some material",
-                shader:shader
+                name: "Some material",
+                shader: shader
             });
         }
 
-        function recalculateNormals(){
+        function recalculateNormals() {
             clearLog();
             var mesh = meshRenderer.mesh;
             mesh.recalculateNormals();
             mesh.updateData();
         }
 
-        function recalculateTangents(){
+        function recalculateTangents() {
             clearLog();
             var mesh = meshRenderer.mesh;
             mesh.recalculateTangents();
             mesh.updateData();
         }
 
-        function addRotatorComponent(gameObject){
+        function addRotatorComponent(gameObject) {
             var time = engine.time,
                 transform = gameObject.transform,
                 rotationSpeed = 0.001,
@@ -66,69 +65,69 @@ requirejs(['kick'],
                 radianToDegree = KICK.core.Constants._RADIAN_TO_DEGREE,
                 res = document.getElementById("res");
             gameObject.addComponent({
-                update: function(){
+                update: function() {
                     var timeTotal = time.time,
-                        rot = timeTotal*rotationSpeed;
-                    translation[0] = Math.sin(rot)*radius;
-                    translation[1] = Math.sin(rot*3);
-                    translation[2] = Math.cos(rot)*radius;
-                    rotVec[1] = rot*radianToDegree;
+                        rot = timeTotal * rotationSpeed;
+                    translation[0] = Math.sin(rot) * radius;
+                    translation[1] = Math.sin(rot * 3);
+                    translation[2] = Math.cos(rot) * radius;
+                    rotVec[1] = rot * radianToDegree;
                     transform.localPosition = translation;
 
                     //new Date().getMilliseconds();
                     transform.localRotationEuler = rotVec;
-                    res.innerHTML = KICK.math.Mat4.strPretty(transform.getGlobalMatrix())+"\nRotation euler:"+KICK.math.Vec3.str(rotVec);
+                    res.innerHTML = KICK.math.Mat4.strPretty(transform.getGlobalMatrix()) + "\nRotation euler:" + KICK.math.Vec3.str(rotVec);
                 }
             });
         }
 
-        function UVSphere(){
+        function UVSphere() {
             meshRenderer.mesh = new KICK.mesh.Mesh({dataURI: "kickjs://mesh/uvsphere/"});
         }
 
-        engine = new KICK.core.Engine('canvas',{
+        engine = new KICK.core.Engine('canvas', {
             enableDebugContext: true
         });
         var cameraObject = engine.activeScene.createGameObject();
         var camera = new KICK.scene.Camera({
-            clearColor: [0,0,0,1],
-            fieldOfView:60,
-            normalizedViewportRect:[0.5,0.5,0.5,0.5]
+            clearColor: [0, 0, 0, 1],
+            fieldOfView: 60,
+            normalizedViewportRect: [0.5, 0.5, 0.5, 0.5]
         });
         cameraObject.addComponent(camera);
 
         var cameraObject2 = engine.activeScene.createGameObject();
         var camera2 = new KICK.scene.Camera({
-            clearColor: [1,1,0,1],
-            fieldOfView:60,
-            cameraIndex:2,
-            normalizedViewportRect:[0.0,0.5,0.5,0.5]
+            clearColor: [1, 1, 0, 1],
+            fieldOfView: 60,
+            cameraIndex: 2,
+            normalizedViewportRect: [0.0, 0.5, 0.5, 0.5]
         });
         cameraObject2.addComponent(camera2);
-        cameraObject2.transform.position = [0,0,3];
+        cameraObject2.transform.position = [0, 0, 3];
         var cameraObject3 = engine.activeScene.createGameObject();
         var camera3 = new KICK.scene.Camera({
-            clearColor: [.1,.3,.6,1],
-            fieldOfView:60,
-            cameraIndex:2,
+            clearColor: [0.1, 0.3, 0.6, 1],
+            fieldOfView: 60,
+            cameraIndex: 2,
             layerMask: 2,
-            normalizedViewportRect:[0.0,0.0,1.0,0.5]
+            normalizedViewportRect: [0.0, 0.0, 1.0, 0.5]
         });
         cameraObject3.addComponent(camera3);
-        cameraObject3.transform.position = [0,0,3];
+        cameraObject3.transform.position = [0, 0, 3];
 
 
         var gameObject = engine.activeScene.createGameObject();
         meshRenderer = new KICK.scene.MeshRenderer();
         meshRenderer.mesh = new KICK.mesh.Mesh({dataURI: "kickjs://mesh/cube/?length=0.5"});
-        setMaterial('vertexShaderColor','fragmentShader',meshRenderer);
+        setMaterial('vertexShaderColor', 'fragmentShader', meshRenderer);
         gameObject.addComponent(meshRenderer);
 
         var gameObject2 = engine.activeScene.createGameObject();
         gameObject2.layer = 2;
         var meshRenderer2 = new KICK.scene.MeshRenderer();
         meshRenderer2.mesh = new KICK.mesh.Mesh({dataURI: "kickjs://mesh/uvsphere/?radius=0.6"});
-        setMaterial('vertexShaderColor','fragmentShader',meshRenderer2);
+        setMaterial('vertexShaderColor', 'fragmentShader', meshRenderer2);
         gameObject2.addComponent(meshRenderer2);
 
         addRotatorComponent(cameraObject);
