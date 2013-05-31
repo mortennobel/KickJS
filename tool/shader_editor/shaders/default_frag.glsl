6c693468e277a939c59630cf9830b421309178d4
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-05-31 13:09:35 +0200

    Update default shader

--- tool/shader_editor/shaders/default_frag.glsl
+++ tool/shader_editor/shaders/default_frag.glsl
@@ -7,13 +7,13 @@ varying vec2 uv;
 uniform sampler2D tex;
 
 
-// pragma include "noise2D.glsl // for snoise(vec2 v)
-// pragma include "noise3D.glsl" //  for snoise(vec3 v)
-// pragma include "noise4D.glsl" //  for snoise(vec4 v)
-// pragma include "cellular2D.glsl" //  for cellular(vec2 P)
-// pragma include "cellular2x2.glsl" //  for cellular2x2(vec2 P)
-// pragma include "cellular2x2x2.glsl" //  for cellular2x2x2(vec3 P)
-// pragma include "cellular3D.glsl" //  cellular(vec3 P)
+//#pragma include "noise2D.glsl // for snoise(vec2 v)
+//#pragma include "noise3D.glsl" //  for snoise(vec3 v)
+//#pragma include "noise4D.glsl" //  for snoise(vec4 v)
+//#pragma include "cellular2D.glsl" //  for cellular(vec2 P)
+//#pragma include "cellular2x2.glsl" //  for cellular2x2(vec2 P)
+//#pragma include "cellular2x2x2.glsl" //  for cellular2x2x2(vec3 P)
+//#pragma include "cellular3D.glsl" //  cellular(vec3 P)
 
 void main(void)
 {
