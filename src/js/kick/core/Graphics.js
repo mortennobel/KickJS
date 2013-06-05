c7cee25b7a1694e4e1c4ae0500b1d42464d4b34a
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2013-06-05 11:11:35 +0200

    Improve documentation and error handling

--- src/js/kick/core/Graphics.js
+++ src/js/kick/core/Graphics.js
@@ -1,9 +1,24 @@
-define(["kick/core/Constants", "kick/scene/Camera", "kick/scene/Transform", "kick/scene/EngineUniforms","kick/scene/MeshRenderer", "kick/math/Mat4", "kick/core/EngineSingleton"],
-    function (constants, Camera, Transform, EngineUniforms, MeshRenderer, Mat4, EngineSingleton) {
+define(["kick/core/Constants", "kick/scene/Camera", "kick/scene/Transform", "kick/scene/EngineUniforms","kick/scene/MeshRenderer", "kick/math/Mat4", "kick/core/EngineSingleton", "kick/texture/RenderTexture", "kick/core/Util", "kick/material/Material"],
+    function (constants, Camera, Transform, EngineUniforms, MeshRenderer, Mat4, EngineSingleton, RenderTexture, Util, Material) {
     "use strict";
-    var ASSERT = constants._ASSERT;
+    var ASSERT = constants._ASSERT,
+        fail = Util.fail;
     return {
         /**
+         *
+         * @example
+         *      // render a unlit shader (with color of red) into a texture
+         *      texture = new kick.texture.Texture();
+         *      texture.setImageData(512, 512, 0, kick.core.Constants.GL_UNSIGNED_BYTE, null, "");
+         *      var renderTexture = new kick.texture.RenderTexture({dimension:[512,512], colorTexture: texture});
+         *      var shader = engine.project.load(engine.project.ENGINE_SHADER_UNLIT);
+         *      var renderMaterial = new kick.material.Material( {
+         *          shader:shader,
+         *          uniformData: {
+         *              mainColor: [1,0,0,1]
+         *          }
+         *      });
+         *      kick.core.Graphics.renderToTexture(renderTexture, renderMaterial);
          * @method renderToTexture
          * @param {kick.texture.RenderTexture} renderTexture
          * @param {kick.material.Material} material
@@ -14,6 +29,14 @@ define(["kick/core/Constants", "kick/scene/Camera", "kick/scene/Transform", "kic
                 engineUniforms,
                 meshRenderer;
             return function(renderTexture, material){
+                if (ASSERT){
+                    if (!(renderTexture instanceof RenderTexture)){
+                        fail("Graphics.renderToTexture: renderTexture must be of type RenderTexture");
+                    }
+                    if (!(material instanceof Material)){
+                        fail("Graphics.renderToTexture: material must be of type Material");
+                    }
+                }
                 if (!camera){
                     engine = EngineSingleton.engine;
                     camera = new Camera({
@@ -57,5 +80,5 @@ define(["kick/core/Constants", "kick/scene/Camera", "kick/scene/Transform", "kic
                 meshRenderer.material = material;
                 meshRenderer.render(engineUniforms);
             }}())
-    }
+        }
     });
