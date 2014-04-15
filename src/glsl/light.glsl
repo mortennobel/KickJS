3c7f3ba1f2664ebcf35e847bd28b62b790065702
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2014-04-15 09:50:00 +0200

    Improve attenuation check

--- src/glsl/light.glsl
+++ src/glsl/light.glsl
@@ -4,22 +4,22 @@ vec3 getPointLightDiffuse(vec3 normal, vec3 ecPosition, mat3 pLights[LIGHTS]){
         vec3 ecLightPos = pLights[i][0]; // light position in eye coordinates
         vec3 colorIntensity = pLights[i][1];
         vec3 attenuationVector = pLights[i][2];
-
-        // direction from surface to light position
-        vec3 VP = ecLightPos -  ecPosition;
-
-        // compute distance between surface and light position
-        float d = length(VP);
-
-        // normalize the vector from surface to light position
-        VP = normalize(VP);
-
-        // compute attenuation
-        float attenuation = 1.0 / dot(vec3(1.0,d,d*d),attenuationVector); // short for constA + liniearA * d + quadraticA * d^2
-
-        float nDotVP = max(0.0, dot(normal, VP));
         bool isLightEnabled = (attenuationVector[0]+attenuationVector[1]+attenuationVector[2])>0.0;
         if (isLightEnabled){
+            // direction from surface to light position
+            vec3 VP = ecLightPos -  ecPosition;
+
+            // compute distance between surface and light position
+            float d = length(VP);
+
+            // normalize the vector from surface to light position
+            VP = normalize(VP);
+
+            // compute attenuation
+            float attenuation = 1.0 / dot(vec3(1.0,d,d*d),attenuationVector); // short for constA + liniearA * d + quadraticA * d^2
+
+            float nDotVP = max(0.0, dot(normal, VP));
+
             diffuse += colorIntensity*nDotVP * attenuation;
         }
     }
@@ -34,31 +34,31 @@ void getPointLight(vec3 normal, vec3 ecPosition, mat3 pLights[LIGHTS],float spec
         vec3 ecLightPos = pLights[i][0]; // light position in eye coordinates
         vec3 colorIntensity = pLights[i][1];
         vec3 attenuationVector = pLights[i][2];
-
-        // direction from surface to light position
-        vec3 VP = ecLightPos -  ecPosition;
-
-        // compute distance between surface and light position
-        float d = length(VP);
-
-        // normalize the vector from surface to light position
-        VP = normalize(VP);
-
-        // compute attenuation
-        float attenuation = 1.0 / dot(vec3(1.0,d,d*d),attenuationVector); // short for constA + liniearA * d + quadraticA * d^2
-
-        vec3 halfVector = normalize(VP + eye);
-
-        float nDotVP = max(0.0, dot(normal, VP));
-        float nDotHV = max(0.0, dot(normal, halfVector));
-        float pf;
-        if (nDotVP <= 0.0){
-            pf = 0.0;
-        } else {
-            pf = pow(nDotHV, specularExponent);
-        }
         bool isLightEnabled = (attenuationVector[0]+attenuationVector[1]+attenuationVector[2])>0.0;
         if (isLightEnabled){
+            // direction from surface to light position
+            vec3 VP = ecLightPos -  ecPosition;
+
+            // compute distance between surface and light position
+            float d = length(VP);
+
+            // normalize the vector from surface to light position
+            VP = normalize(VP);
+
+            // compute attenuation
+            float attenuation = 1.0 / dot(vec3(1.0,d,d*d),attenuationVector); // short for constA + liniearA * d + quadraticA * d^2
+
+            vec3 halfVector = normalize(VP + eye);
+
+            float nDotVP = max(0.0, dot(normal, VP));
+            float nDotHV = max(0.0, dot(normal, halfVector));
+            float pf;
+            if (nDotVP <= 0.0){
+                pf = 0.0;
+            } else {
+                pf = pow(nDotHV, specularExponent);
+            }
+
             diffuse += colorIntensity * nDotVP * attenuation;
             specular += pf * attenuation;
         }
